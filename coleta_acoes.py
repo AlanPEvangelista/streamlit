8cfee5c5e306142725b49035dbc671107579d520
AlanPEvangelista <138069995+AlanPEvangelista@users.noreply.github.com>
2024-02-03 22:33:32 -0300

    Add files via upload

--- coleta_acoes.py
+++ coleta_acoes.py
@@ -2,9 +2,9 @@ import streamlit as st
 import yfinance as yf
 
 st.title(''':blue[Preço de Ativo]''')
-st.header("Informações a respeito do fechamento e volume de algumas ações")
+st.header("Informações de ações deste 01/01/2012")
 st.markdown('''
-    :red[Ativo PETR4] :orange[Temos] :green[outros] :blue[gráficos].''')
+    :orange[Temos] :green[outros] :blue[gráficos].''')
 
 # ENBR3.SA
 # BBAS3.SA
@@ -12,9 +12,13 @@ st.markdown('''
 # PETR4.SA
 # VALE5.SA
 
-tickerSimbolo = "PETR4.SA"
+opcoes = st.selectbox('Escolha o Ativo', ('ENBR3.SA',
+                      'BBAS3.SA', 'BBDC4.SA', 'PETR4.SA', 'VALE5.SA'))
+
+tickerSimbolo = opcoes
+
 tickerData = yf.Ticker(tickerSimbolo)
-tikerDf = tickerData.history(period='1d', start='2022-1-1')
+tikerDf = tickerData.history(period='1d', start='2012-1-1')
 st.header("Grafico de Fechamento")
 st.line_chart(tikerDf.Close)
 st.header("Gráfico de Volume")
